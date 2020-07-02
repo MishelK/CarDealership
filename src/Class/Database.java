b84ea85b92a6990356fcc015b8dfae04f56ad6d5
MishelK <brickpedit@gmail.com>
2020-07-02 22:36:45 +0300

    Database Final Fix
    database is not working with newest connector

--- src/Class/Database.java
+++ src/Class/Database.java
@@ -5,12 +5,12 @@ public class Database {
 	
 	public static void main(String[] args) throws Exception
 	{
-		String url = "jdbc:mysql://localhost:3306/dealership";
+		String url = "jdbc:mysql://localhost:3306/dealership?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=GMT";
 		String username = "root";
 		String pass = "root";
 		String query = "select username from staff where staffid=1";
 		
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection(url, username, pass);
 		
 		Statement st = con.createStatement();
